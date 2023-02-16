055cd12b411afe48957976a7e97d75c3b4cb170d
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 00:03:11 +0200

    imports

--- src/contracts/BondingCurveCos.sol
+++ src/contracts/BondingCurveCos.sol
@@ -11,7 +11,7 @@ pragma solidity 0.8.17;
 
 import "./MISC.sol";
 import "./UniswapRouter.sol";
-import "./MarketTransition.sol";
+import "./MarketTransition.sol"; 
 import "../libraries/LibErrors.sol";
 import "../libraries/LibEvents.sol";
 import "@prb-math/sd59x18/Math.sol";
