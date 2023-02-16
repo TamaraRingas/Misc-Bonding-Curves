2afa2a72d90afa456f72216bce5ac59aa9eff055
Tamara Ringas <tamriasdev@gmail.com>
2023-02-16 23:12:20 +0200

    update imports

--- src/contracts/BondingCurvePiecewise.sol
+++ src/contracts/BondingCurvePiecewise.sol
@@ -6,7 +6,12 @@ pragma solidity 0.8.17;
  / /_)/| | / _ \ / __| / _ \ _____ \ \ /\ / /| |/ __| / _ \
 / ___/ | ||  __/| (__ |  __/|_____| \ V  V / | |\__ \|  __/
 \/     |_| \___| \___| \___|         \_/\_/  |_||___/ \___|
-*/                                                          
+*/
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@prb-math/sd59x18/Math.sol";  
 
 contract BondingCurvePiecewise {
 
