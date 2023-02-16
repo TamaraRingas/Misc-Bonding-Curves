56e4b7dc1d40774ec1c87dfdf01b8414ce0b7d2c
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 01:07:44 +0200

    Instantiate contract

---
+++ src/contracts/Curve.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+/*
+   ___           __              __ 
+  / __\ /\ /\   /__\  /\   /\   /__\
+ / /   / / \ \ / \//  \ \ / /  /_\  
+/ /___ \ \_/ // _  \   \ V /  //__  
+\____/  \___/ \/ \_/    \_/   \__/  
+*/
+
+import "../interfaces/ICurve.sol";
+
+contract Curve is ICurve {
+
+}
