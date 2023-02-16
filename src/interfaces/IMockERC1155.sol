244a7f476d09372b7df37ec6c062dd9714e09d2b
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 01:28:35 +0200

    update mintBatch()

--- src/interfaces/IMockERC1155.sol
+++ src/interfaces/IMockERC1155.sol
@@ -9,28 +9,20 @@ pragma solidity 0.8.17;
 \__/  \/ \_/ \____/   |_||_||____/ |____/                                      
 */
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "../interfaces/IMockERC1155.sol";
 
-contract NFT is ERC1155, IMockERC1155 {
-
-    constructor() ERC1155("ipfs://baseURI") {}
+interface IMockERC1155 {
 
     function mint(
         address _to,
         uint256 _id,
         uint256 _amount,
         bytes memory _data
-    ) public {
-        _mint(_to, _id, _amount, _data);
-    }
+    ) external;
 
     function mintBatch(
         address _to,
         uint256[] memory _ids,
         uint256[] memory _amounts,
         bytes memory _data
-    ) public {
-        _mintBatch(_to, _ids, _amounts, _data);
-    }
+    ) external;
 }
