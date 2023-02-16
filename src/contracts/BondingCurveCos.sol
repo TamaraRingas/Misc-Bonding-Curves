9a450e6a9fb9bab72d970b07948ea49450479e55
Tamara Ringas <tamriasdev@gmail.com>
2023-02-16 16:34:18 +0200

    comments

--- src/contracts/BondingCurveCos.sol
+++ src/contracts/BondingCurveCos.sol
@@ -3,6 +3,13 @@ pragma solidity 0.8.17;
 
 import "../interfaces/IBondingCurveCos.sol";
 
+/*
+  ___   ___   ___ 
+ / __| / _ \ / __|
+| (__ | (_) |\__ \
+ \___| \___/ |___/              
+*/
+
 contract BondingCurveCos {
 
 }
