e1bf71c40a2e6611714a195bf213b3b8017d167c
Tamara Ringas <tamriasdev@gmail.com>
2023-02-16 23:12:12 +0200

    update imports

--- src/contracts/BondingCurveCos.sol
+++ src/contracts/BondingCurveCos.sol
@@ -14,8 +14,8 @@ import "./MISC.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@prb-math/SD59x18.sol"; 
+import "@prb-math/sd59x18/Math.sol";  
 
 contract BondingCurveCos is IBondingCurveCos {
-
+  //using  for int256;
 }
