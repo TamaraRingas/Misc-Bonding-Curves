7d80595f5870672f81c70779537b9716f94f969f
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 18:42:13 +0200

    imports

--- src/contracts/CurveFactory.sol
+++ src/contracts/CurveFactory.sol
@@ -18,8 +18,8 @@ import "../libraries/LibEvents.sol";
 import "@prb-math/sd59x18/Math.sol";
 import "../interfaces/ICurveFactory.sol"; 
 import "../interfaces/IUniswapRouter.sol"; 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CurveFactory is ICurveFactory {
   
