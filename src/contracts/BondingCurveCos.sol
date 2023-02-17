d5457eb54d357c75d1f7140fb5c6f6d76587927a
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 22:04:24 +0200

    add setCurveAddress()

--- src/contracts/BondingCurveCos.sol
+++ src/contracts/BondingCurveCos.sol
@@ -9,8 +9,8 @@ pragma solidity 0.8.17;
 ****************************************************/
 
 import "./MISC.sol";
-import "./UniswapRouter.sol";
-import "./MarketTransition.sol"; 
+import "./MarketTransition.sol";
+import "../interfaces/ICurve.sol"; 
 import "../libraries/LibErrors.sol";
 import "../libraries/LibEvents.sol";
 import "@prb-math/sd59x18/Math.sol";
@@ -20,167 +20,14 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  
 
 contract BondingCurveCos is IBondingCurveCos, Ownable {
-  //using  for int256;
 
-  // =================== VARIABLES =================== //
+  address curve;
 
-  int256 immutable maxThreshold;
-  int256 immutable minThreshold;
-  int256 tokensSold;
-
-  uint256 startTime;
-  uint256 immutable timeoutPeriod;
-  uint256 timeoutPeriodExpiry;
-
-  bool public curveActive; // ToDo: Change to uint8 to save space
-  bool public transitionConditionsMet; // ToDo: Change to uint8 
-  bool public transitioned; // ToDo: Change to uint8 
-  bool public curveInitialized;
-  bool public nftAccess;
-
-  address uniswapRouter;
-  address marketTransition;
-  address treasury;
-
-  IERC20 COLL;
-  MISC misc;
-
-  // =================== MODIFIERS =================== //
-
-  modifier unitialized() {
-    if (curveInitialized == true) revert LibErrors.CurveInitialized(); 
-    _;
-  }
-    
-  modifier isActive() {
-    if (curveActive == false) revert LibErrors.CurvePaused();
-    _;
+  function setCurveAddress(address _curveAddress) onlyOwner external {
+      curve = _curveAddress;
   }
 
-   /// @notice Checks if a user is whitelisted for the current sale round
-    /// @dev Gets the currentNFTStage and checks if the user has a balance of the correcponding NFT in their wallet
-    modifier isEligible() {
-        if (nftAccess) {
-            if (keccak256(bytes(currentNFTStage)) == keccak256(bytes("Black"))) {
-            require(NFT.balanceOf(msg.sender, BLACK_NFT_ID) > 0, "NFTRequired");
-            _;
-        }
-        if (keccak256(bytes(currentNFTStage)) == keccak256(bytes("Gold"))) {
-            require(
-                NFT.balanceOf(msg.sender, BLACK_NFT_ID) > 0 ||
-                    NFT.balanceOf(msg.sender, GOLD_NFT_ID) > 0,
-                "NFTRequired"
-            );
-            _;
-        }
-        if (keccak256(bytes(currentNFTStage)) == keccak256(bytes("Silver"))) {
-            require(
-                NFT.balanceOf(msg.sender, BLACK_NFT_ID) > 0 ||
-                    NFT.balanceOf(msg.sender, GOLD_NFT_ID) > 0 ||
-                    NFT.balanceOf(msg.sender, SILVER_NFT_ID) > 0,
-                "NFTRequired"
-            );
-            _;
-        }
-        if (keccak256(bytes(currentNFTStage)) == keccak256(bytes("None"))) {
-            _;
-        }
-      }
-      _;
-    }
-
-   // =================== CONSTRUCTOR =================== //
-
-    constructor(
-        address _collateralAddress,
-        address _miscAddress,
-        address _nftAddress,
-        address _treasuryAddress,
-        address _uniswapRouter,
-        address _marketTransition
-    ) {
-
-        maxThreshold = 20000000; // ToDo set this in initialize function
-        minThreshold = 5000000; // ToDo set this in initialize function
-        timeoutPeriod = 150 days;// ToDo set this in initialize function
-
-        tokensSold = 0;
-
-        treasury = _treasuryAddress;
-        uniswapRouter = _uniswapRouter;
-        marketTransition = _marketTransition;
-        //getPriceContract = IGetPrice(_getPrice);
-
-        COLL = IERC20(_collateralAddress);
-        misc = MISC(_miscAddress);
-
-        //NFT = IERC1155(_nftAddress);
-
-        curveActive = false; // ToDo switch to uint8
-        transitionConditionsMet = false; // ToDo switch to uint8
-        transitioned = false; // ToDo switch to uint8
-
-        //nftStage = NFTStage(true, false, false, false);
-        //currentNFTStage = "Black";
-    }
-
-    // =================== OWNER FUNCTIONS =================== //
-
-    function initializeCurve() external onlyOwner unitialized() {
-  
-        startTime = block.timestamp;
-        activateCurve();
-        emit LibEvents.CurveActivated(msg.sender, block.timestamp);
-    }
     
-    function pauseCurve() external {
-        marketTransition = curveFactory.getMarketAddress(
-            address(this)
-        );
-        require(
-            msg.sender == marketTransition ||
-            msg.sender == owner(),
-            "Access Denied"
-        );
-
-        curveActive = false;
-
-        emit LibEvents.CurvePaused(msg.sender, block.timestamp);
-    }
-    
-    function activateCurve() public onlyOwner {
-        curveActive = true;
-        timeoutPeriodExpiry = startTime + timeoutPeriod;
-        emit LibEvents.CurveActivated(msg.sender, block.timestamp);
-    }
-
-    // =================== VIEW FUNCTIONS =================== //
-
-    // function getFee(int256 _price) public pure returns (int256) {
-    //     int256 fee = PRBMathSD59x18.mul(
-    //         PRBMathSD59x18.div(5 * 1e18, 100e18),
-    //         _price
-    //     );
-    //     return fee;
-    // }
-
-    // function getMarketTransitionContractAddress()
-    //     public
-    //     view
-    //     returns (address)
-    // {
-    //     return curveFactory.getMarketAddress(address(this));
-    // }
-
-    // =================== GENERAL FUNCTIONS =================== //
-
-    function buyMISC(uint256 amount) external isActive {
-
-    }
-
-    function sellMISC(uint256 amount) external isActive {
-
-    }
 }
 
 
