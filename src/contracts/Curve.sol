fd0ff6a9537c84beca309e1d5bcd6a318c7bc31c
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 18:35:29 +0200

    update vars

--- src/contracts/Curve.sol
+++ src/contracts/Curve.sol
@@ -12,5 +12,27 @@ pragma solidity 0.8.17;
 import "../interfaces/ICurve.sol"; 
 
 contract Curve is ICurve {
+  
+  // =================== VARIABLES =================== //
 
+  int256 immutable maxThreshold;
+  int256 immutable minThreshold;
+  int256 tokensSold;
+
+  uint256 startTime;
+  uint256 immutable timeoutPeriod;
+  uint256 timeoutPeriodExpiry;
+
+  bool public curveActive; // ToDo: Change to uint8 to save space
+  bool public transitionConditionsMet; // ToDo: Change to uint8 
+  bool public transitioned; // ToDo: Change to uint8 
+  bool public curveInitialized;
+  bool public nftAccess;
+
+  address uniswapRouter;
+  address marketTransition;
+  address treasury;
+
+  IERC20 COLL;
+  MISC misc;
 }
