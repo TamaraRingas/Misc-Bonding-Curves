7c546f663ca84cf182fa792c09053877b76c8ee9
Tamara Ringas <tamriasdev@gmail.com>
2023-02-17 18:36:26 +0200

    remove unused import

--- src/contracts/BondingCurveCos.sol
+++ src/contracts/BondingCurveCos.sol
@@ -17,7 +17,6 @@ import "@prb-math/sd59x18/Math.sol";
 import "../interfaces/IBondingCurveCos.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  
 
 contract BondingCurveCos is IBondingCurveCos, Ownable {
