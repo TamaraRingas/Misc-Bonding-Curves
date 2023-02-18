38b60040c92600d5889c0f42506b6ebe13b4e88b
Tamara Ringas <tamriasdev@gmail.com>
2023-02-18 17:48:27 +0200

    Update price func params

--- src/interfaces/IBondingCurveLn.sol
+++ src/interfaces/IBondingCurveLn.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+/*
+ _             ___                           
+| | _ __      / __\ _   _  _ __ __   __  ___ 
+| || '_ \    / /   | | | || '__|\ \ / / / _ \
+| || | | |  / /___ | |_| || |    \ V / |  __/
+|_||_| |_|  \____/  \__,_||_|     \_/   \___|
+********************************************/
+
 interface IBondingCurveLn {
+  
+  function setCurveAddress(address curveInstance) external;
+
+  function calculatePirceSinCurve(uint256 amountMISC) external returns (uint256 price);
   
 }
