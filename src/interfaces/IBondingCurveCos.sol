aef296bd7c6b5e77bc26d9d827c37e9bfe154088
Tamara Ringas <tamriasdev@gmail.com>
2023-02-18 17:38:17 +0200

    add funcs

--- src/interfaces/IBondingCurveCos.sol
+++ src/interfaces/IBondingCurveCos.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.17;
 
 interface IBondingCurveCos {
   
+  function setCurveAddress(address curveInstance) external;
+
+  function calculatePirceSinCurve(uint256 amountMISC) external returns (uint256 price);
+  
 }
