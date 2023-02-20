1220c257cc4e37ee669c8149bcb99c3765f360de
Tamara Ringas <tamriasdev@gmail.com>
2023-02-20 18:26:03 +0200

    imports

--- src/contracts/MISC.sol
+++ src/contracts/MISC.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol"; 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /*                       _____         _                 
  _ __ ___   ___   ___   /__   \  ___  | | __  ___  _ __  
