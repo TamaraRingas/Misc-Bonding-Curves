1096ca897919727445cbaeda62b779f11ee47ec1
Tamara Ringas <tamriasdev@gmail.com>
2023-02-20 18:27:39 +0200

    imports

--- src/contracts/Vesting.sol
+++ src/contracts/Vesting.sol
@@ -9,14 +9,12 @@ pragma solidity 0.8.17;
    \_/   \__/  \__/    \/    \____/  \_\ \/  \____/ 
 */
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract TokenVesting is Ownable, ReentrancyGuard {
-    using SafeMath for uint256;
 
     struct VestingSchedule {
         bool initialized;
