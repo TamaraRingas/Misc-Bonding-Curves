87a6980e2d9adcb0bc73914ce547b52dfd32dc52
Tamara Ringas <tamriasdev@gmail.com>
2023-02-25 14:26:40 +0200

    rename contract

---
+++ src/contracts/SinFormula.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+/*    _            ___                           
+ ___ (_) _ __     / __\ _   _  _ __ __   __  ___ 
+/ __|| || '_ \   / /   | | | || '__|\ \ / / / _ \
+\__ \| || | | | / /___ | |_| || |    \ V / |  __/
+|___/|_||_| |_| \____/  \__,_||_|     \_/   \___|
+****************************************************/
+
+import "./MarketTransition.sol";
+import "../interfaces/ICurve.sol"; 
+import "../libraries/LibErrors.sol";
+import "../libraries/LibEvents.sol";
+import "@prb-math/sd59x18/Math.sol";
+import "@trigonometry/Trigonometry.sol";
+import "../interfaces/ISinFormula.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SinFormula is IBondingCurveSin, Ownable { 
+   
+  address curve;
+
+  // ToDo change from Ownable calling this func to CurveFactory calling when creating a new curve instance with this as its price calculator
+  function setCurveAddress(address _curveAddress) onlyOwner external {
+      curve = _curveAddress;
+  }
+
+  /// @notice Determines the price for an input amount of MISC, in COLL.
+  /// @dev The price is calculated as the integral between the endPoint(The amount of tokens sold after the user has bought the input amount) and the startPoint(the amount of tokens bought before the quote was requested) along the shifted cos curve.
+  /// @param _amountMISC - The amount of MISC the user wishes to recieve a quote for in COLL. 
+  /// @param _tokenDecimals - The amount of decimals of the input token, 18 for ETH and 6 for USDC. 
+  /// @return price - The amount of COLL (scaled) to be transferred if the input amount of MISCtokens are bought.
+  function calculatePriceSin(uint256 _amountMISC, uint8 _tokenDecimals) 
+            external 
+            view 
+            returns(int256 price) {
+
+  }
+
+    
+}
