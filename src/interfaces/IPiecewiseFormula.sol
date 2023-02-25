f18601bae195d58e253367814925692edd746e89
Tamara Ringas <tamriasdev@gmail.com>
2023-02-25 14:25:56 +0200

    rename contract

---
+++ src/interfaces/IPiecewiseFormula.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+/* ___  _                                     _            
+  / _ \(_)  ___   ___   ___        __      __(_) ___   ___ 
+ / /_)/| | / _ \ / __| / _ \ _____ \ \ /\ / /| |/ __| / _ \
+/ ___/ | ||  __/| (__ |  __/|_____| \ V  V / | |\__ \|  __/
+\/     |_| \___| \___| \___|         \_/\_/  |_||___/ \___|
+*********************************************************/
+
+interface IBondingCurvePiecewise {
+
+  function setCurveAddress(address curveInstance) external;
+
+  function calculatePricePiecewise(uint256 amountMISC, uint8 tokenDecimals) external view returns (int256 price);
+} 
