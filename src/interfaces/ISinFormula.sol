8c991beccf872a8da0b973e001dadc630cbfa6a1
Tamara Ringas <tamriasdev@gmail.com>
2023-02-25 14:26:02 +0200

    rename contract

---
+++ src/interfaces/ISinFormula.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+/*    _            ___                           
+ ___ (_) _ __     / __\ _   _  _ __ __   __  ___ 
+/ __|| || '_ \   / /   | | | || '__|\ \ / / / _ \
+\__ \| || | | | / /___ | |_| || |    \ V / |  __/
+|___/|_||_| |_| \____/  \__,_||_|     \_/   \___|
+****************************************************/
+
+interface IBondingCurveSin {
+
+  function setCurveAddress(address curveInstance) external;
+
+  function calculatePriceSin(uint256 amountMISC, uint8 tokenDecimals) external view returns (int256 price);
+
+}
