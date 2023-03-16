898efbc1fb13eac21d7e2c8f639e1e192c87fd87
Tamara Ringas <tamriasdev@gmail.com>
2023-03-16 19:06:53 +0200

    imports

--- src/contracts/CosFormula2.sol
+++ src/contracts/CosFormula2.sol
@@ -7,3 +7,14 @@ pragma solidity 0.8.17;
 | (__ | (_) |\__ \  / /___ | |_| || |    \ V / |  __/
  \___| \___/ |___/  \____/  \__,_||_|     \_/   \___|
 ****************************************************/
+
+import "./MISC.sol";
+import "./MarketTransition.sol";
+import "../interfaces/ICurve.sol"; 
+import "../libraries/LibErrors.sol";
+import "../libraries/LibEvents.sol";
+import "@prb-math/sd59x18/Math.sol";
+import "@trigonometry/Trigonometry.sol";
+import "../interfaces/ICosFormula1.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
