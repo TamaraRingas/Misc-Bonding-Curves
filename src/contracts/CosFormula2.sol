61177caa4613226662a0c29c3fdc17eff3623c69
Tamara Ringas <tamriasdev@gmail.com>
2023-03-16 19:13:10 +0200

    name

--- src/contracts/CosFormula2.sol
+++ src/contracts/CosFormula2.sol
@@ -19,7 +19,7 @@ import "../interfaces/ICosFormula2.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract CosFormula1 is ICosFormula1, Ownable { 
+contract CosFormula2 is ICosFormula1, Ownable { 
 
   address curve;
 
