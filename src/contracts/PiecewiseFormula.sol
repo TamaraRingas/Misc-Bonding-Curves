31936f5bbef215e424696e5ac94e10c137a00a5a
Tamara Ringas <tamriasdev@gmail.com>
2023-05-16 14:32:11 +0200

    comments

--- src/contracts/PiecewiseFormula.sol
+++ src/contracts/PiecewiseFormula.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.17; 
 
 /* ___  _                                     _            
   / _ \(_)  ___   ___   ___        __      __(_) ___   ___ 
